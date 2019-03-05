23b77782eca0fc8fd4f411a28ed1623da04d6a90
D-rClef <43148218+D-rClef@users.noreply.github.com>
2019-03-06 00:57:31 +0300

    .

--- 03.03.19/№3/№3.cpp
+++ 03.03.19/№3/№3.cpp
@@ -9,55 +9,70 @@ struct Node
 	Node* next = NULL;
 	Node* prev = NULL;
 };
-class char_deque {
+
+class char_deque
+{
 	Node* head;
 	Node* tail;
 public:
 	char_deque(): head(NULL){}
-	void push_front(char x) {
-		if (head == NULL) {
+
+	void push_front(char x)
+	{
+		if (head == NULL)
+        {
 			head = new Node;
 			tail = head;
 			head->data = x;
-		}
+		};
 		head->prev = new Node;
 		head->prev->next = head;
 		head = head->prev;
 		head->data = x;
-	}
-	void push_back(char x) {
-		if (head == NULL) {
+	};
+
+	void push_back(char x)
+	{
+		if (head == NULL)
+        {
 			head = new Node;
 			tail = head;
 			head->data = x;
-		}
+		};
 		tail->next = new Node;
 		tail->next->prev = tail;
 		tail = tail->next;
 		tail->data = x;
-	}
-	char pop_front() {
-		if (head == NULL) {
+	};
+
+	char pop_front()
+	{
+		if (head == NULL)
+        {
 			cout << "Deque is empty";
 			return 0;
-		}
+		};
 		Node* temp = head;
 		head = head->next;
 		char x = temp->data;
 		delete temp;
 		return x;
-	}
-	char pop_back() {
-		if (head == NULL) {
+	};
+
+	char pop_back()
+	{
+		if (head == NULL)
+        {
 			cout << "Deque is empty";
 			return 0;
-		}
+		};
 		Node* temp = tail;
 		tail = tail->prev;
 		char x = temp->data;
 		delete temp;
 		return x;
-	}
+	};
+
 	~char_deque()
 	{
 		while (head != NULL)
@@ -65,19 +80,20 @@ public:
 			Node* temp = head;
 			head = head->next;
 			delete temp;
-		}
-	}
+		};
+	};
 };
-int main() {
+
+int main()
+{
 	char_deque A;
-	A.push_front('a');
-	A.push_front('b');
-	cout << A.pop_front() << "\n";
-	cout << A.pop_front() << "\n";
-	A.push_back('c');
-	A.push_back('d');
-	A.push_back('f');
-	cout << A.pop_back() << "\n";
-	cout << A.pop_back() << "\n";
+	A.push_front('9');
+	A.push_front('1');
+	cout << A.pop_front() << " ";
+	cout << A.pop_front() << " ";
+	A.push_back('4');
+	A.push_back('8');
+	cout << A.pop_back() << " ";
+	cout << A.pop_back() << " ";
 	return 0;
 }
